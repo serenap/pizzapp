0725f5f2de65f31cefdb8abef2c94bde1da37fb8
filippotp <filippotp@gmail.com>
2014-07-09 16:02:13 +0200

    Aggiunte classi View sulla base dei template creati

---
+++ js/views/pages/CartoneView.js
@@ -0,0 +1,51 @@
+define(function(require) {
+
+  var Backbone = require("backbone");
+  var MyModel = require("models/MyModel");
+  var Utils = require("utils");
+
+  var CartoneView = Utils.Page.extend({
+
+    constructorName: "CartoneView",
+
+    model: MyModel,
+    
+    initialize: function() {
+      // load the precompiled template
+      this.template = Utils.templates.cartone;
+
+      // here we can register to inTheDOM or removing events
+      // this.listenTo(this, "inTheDOM", function() {
+      //   $('#content').on("swipe", function(data){
+      //     console.log(data);
+      //   });
+      // });
+      // this.listenTo(this, "removing", functionName);
+
+      // by convention, all the inner views of a view must be stored in this.subViews
+    },
+
+    id: "cartone",
+    //className: "i-g page",
+
+    events: {
+    
+
+    },   
+
+
+    render: function() {
+       // load the template
+      this.el.innerHTML = this.template({});
+      // cache a reference to the content element
+      this.contentElement = this.$el.find('#content')[0];
+     //$(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+  
+  });
+
+  return CartoneView;
+
+});
---
+++ js/views/pages/HomeView.js
@@ -0,0 +1,51 @@
+define(function(require) {
+
+  var Backbone = require("backbone");
+  var MyModel = require("models/MyModel");
+  var Utils = require("utils");
+
+  var HomeView = Utils.Page.extend({
+
+    constructorName: "HomeView",
+
+    model: MyModel,
+    
+    initialize: function() {
+      // load the precompiled template
+      this.template = Utils.templates.home;
+
+      // here we can register to inTheDOM or removing events
+      // this.listenTo(this, "inTheDOM", function() {
+      //   $('#content').on("swipe", function(data){
+      //     console.log(data);
+      //   });
+      // });
+      // this.listenTo(this, "removing", functionName);
+
+      // by convention, all the inner views of a view must be stored in this.subViews
+    },
+
+    id: "home",
+    //className: "i-g page",
+
+    events: {
+    
+
+    },   
+
+
+    render: function() {
+       // load the template
+      this.el.innerHTML = this.template({});
+      // cache a reference to the content element
+      this.contentElement = this.$el.find('#content')[0];
+     //$(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+
+  
+  });
+
+  return HomeView;
+
+});
