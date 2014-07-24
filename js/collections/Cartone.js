7bb766a825666e66a609381e0bd7520e5ce27339
filippotp <filippotp@gmail.com>
2014-07-24 17:57:53 +0200

    Completato metodo aggiungiPizza di Cartone

--- js/collections/Cartone.js
+++ js/collections/Cartone.js
@@ -8,19 +8,27 @@ define(function(require) {
 		model : Pizza,
 
 		aggiungiPizza: function(nuova_pizza){
-			//var presente = false;	
-			this.each(function(pizza){
-				if(pizza.get("nome") == nuova_pizza.get("nome") && pizza.get("ingredienti") == nuova_pizza.get("ingredienti")){
-					pizza.set("quantita", pizza.get("quantita")++);
-					pizza.get("prezzo", pizza.get("prezzo")*=pizza.get("quantita"));
-					console.log("presente");
+			var presente = false;
+			var i = 0;
+			while(!presente && i<this.length){
+				var pizza = this.at(i);
+				if((pizza.get("nome") == nuova_pizza.get("nome")) && 
+					(JSON.stringify(pizza.get("ingredienti")) == JSON.stringify(nuova_pizza.get("ingredienti")))){
+					var quantita = pizza.get("quantita");
+					quantita++;
+					
+					var nuovo_prezzo = nuova_pizza.get("prezzo")*(quantita);
+
+					pizza.set("quantita", quantita);
+					pizza.set("prezzo", nuovo_prezzo);
+					presente = true;
 				}
-			});
-			/*if(!presente){
+				i++;
+			}
+			if(!presente){
 				nuova_pizza.set({"quantita": 1});
 				this.push(nuova_pizza);
-				console.log("assente");
-			}*/
+			}
 		},
 
 		rimuoviPizza: function(pizza){
